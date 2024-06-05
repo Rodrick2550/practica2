38d7479fcb6bd4421dde6dd3ae07e8949a54ca4c
Rodrigo Ruiz García <rodrick@MacBook-Air-de-Rodrigo.local>
2024-06-05 14:18:44 -0600

    fourth commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-  res.send('¡Hola Mundo mampo!');
+  res.send('¡Hola mampo!');
 });
 
 app.listen(PORT, () => {
